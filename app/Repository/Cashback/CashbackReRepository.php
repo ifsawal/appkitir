fa4dbd2aaaddf31648bcc7dc66ad34fab7219fe5
ifsawal <ifsawal@gmail.com>
2024-05-30 16:14:57 +0700

    ubah tampilan status bayar

--- app/Repository/Cashback/CashbackReRepository.php
+++ app/Repository/Cashback/CashbackReRepository.php
@@ -40,7 +40,7 @@ class CashbackReRepository
             ->where('bulan',$bulan)
             ->where('tahun',$tahun)
             ->first();
-            if($syarat_cek && $p['status']==NULL){$syarat="<font color=#009349><b>Syarat &#x2705 </b></font>";}else{$syarat="";}
+            if($syarat_cek && $p['bayar']==NULL){$syarat="<font color=#009349><b>Syarat &#x2705 </b></font>";}else{$syarat="";}
             $penjualan = KitirPenjualan::whereYear('tanggal', '=', $tahun)
                 ->join('cashback', 'kitir_penjualan.id', '=', 'cashback.kitir_penjualan_id')
                 ->whereMonth('tanggal', '=', $bulan)
